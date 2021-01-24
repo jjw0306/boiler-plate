13da2834beda4879b8fe67fd5b20d559ce42c8fa
Juwon <jjw036@gmail.com>
2021-01-24 23:05:27 +0900

    #34 끝!

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { loginUser } from '../../../_actions/user_action';
 
 function LoginPage(props) {
@@ -56,4 +57,4 @@ function LoginPage(props) {
   );
 }
 
-export default LoginPage;
+export default withRouter(LoginPage);
